e685457d965c88d656343a2476411ff6711e0d69
Yutthana L <ultraspacex@gmail.com>
2018-09-15 00:18:40 +0700

    update full solution

---
+++ GoogleMapAPI-Distance/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace GoogleMapAPI_Distance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbDistance = new System.Windows.Forms.Label();
+            this.lbDestination = new System.Windows.Forms.Label();
+            this.lnOrigins = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(586, 242);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "    Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 169);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(291, 29);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "13.6899991,100.7479237";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 142);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Origin : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(434, 142);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Destination : ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(438, 169);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(291, 29);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "13.9132602,100.60201";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Open Sans", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(63)))), ((int)(((byte)(182)))));
+            this.label7.Location = new System.Drawing.Point(95, 9);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(653, 65);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Distance Matrix API | DEMO";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(79, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(47, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 22);
+            this.label1.TabIndex = 16;
+            this.label1.Text = " example : 13.9213371,100.5753477";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(434, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 22);
+            this.label2.TabIndex = 17;
+            this.label2.Text = " example :  13.6899991,100.7479237";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(361, 169);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(57, 34);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lbTime);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.lbDistance);
+            this.panel1.Controls.Add(this.lbDestination);
+            this.panel1.Controls.Add(this.lnOrigins);
+            this.panel1.Location = new System.Drawing.Point(52, 316);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(677, 302);
+            this.panel1.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Location = new System.Drawing.Point(18, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(640, 1);
+            this.label3.TabIndex = 29;
+            // 
+            // lbTime
+            // 
+            this.lbTime.BackColor = System.Drawing.Color.Transparent;
+            this.lbTime.Font = new System.Drawing.Font("Open Sans", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTime.Location = new System.Drawing.Point(392, 240);
+            this.lbTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(203, 39);
+            this.lbTime.TabIndex = 28;
+            this.lbTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(392, 209);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(86, 22);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Duration :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(83, 209);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(84, 22);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Distance :";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(16, 106);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(106, 22);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Destination :";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(15, 30);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 22);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Origin :";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(51, 291);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 22);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Result :";
+            // 
+            // lbDistance
+            // 
+            this.lbDistance.BackColor = System.Drawing.Color.Transparent;
+            this.lbDistance.Font = new System.Drawing.Font("Open Sans", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDistance.Location = new System.Drawing.Point(83, 240);
+            this.lbDistance.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbDistance.Name = "lbDistance";
+            this.lbDistance.Size = new System.Drawing.Size(169, 39);
+            this.lbDistance.TabIndex = 22;
+            this.lbDistance.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbDestination
+            // 
+            this.lbDestination.BackColor = System.Drawing.Color.Transparent;
+            this.lbDestination.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDestination.ForeColor = System.Drawing.Color.Gray;
+            this.lbDestination.Location = new System.Drawing.Point(130, 106);
+            this.lbDestination.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbDestination.Name = "lbDestination";
+            this.lbDestination.Size = new System.Drawing.Size(528, 64);
+            this.lbDestination.TabIndex = 21;
+            // 
+            // lnOrigins
+            // 
+            this.lnOrigins.BackColor = System.Drawing.Color.Transparent;
+            this.lnOrigins.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnOrigins.ForeColor = System.Drawing.Color.Gray;
+            this.lnOrigins.Location = new System.Drawing.Point(130, 30);
+            this.lnOrigins.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lnOrigins.Name = "lnOrigins";
+            this.lnOrigins.Size = new System.Drawing.Size(528, 65);
+            this.lnOrigins.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(795, 659);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Google Distance Metrix API";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbDistance;
+        private System.Windows.Forms.Label lbDestination;
+        private System.Windows.Forms.Label lnOrigins;
+    }
+}
+
---
+++ GoogleMapAPI-Distance/Form1.cs
@@ -0,0 +1,144 @@
+﻿// Yutthana L. 
+// ultraspacex@gmail.com
+// 14-9-2018
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace GoogleMapAPI_Distance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Mouse drag form
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        // Form Drop shadow
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int csDropshadow = 0x00020000;
+                var cp = base.CreateParams;
+                cp.ClassStyle |= csDropshadow;
+                return cp;
+            }
+        }
+
+        public async void GetSingleDistanceFromGoogleMapApi(string origins, string destination)
+        {
+            const string googleMapApiKey = "AIzaSyB9A_Z9HMQNbqHLUmSNyhR4bIDOEg2IiRY";
+            const string units = "metric";
+            const string avoids = "tolls,highways,indoor";
+            const string language = "en-US";
+
+            var client = new RestClient(
+                "https://maps.googleapis.com/maps/api/distancematrix/json?mode=driving&origins=" + origins +
+                "&destinations=" + destination +
+                "&language=" + language +
+                "&key=" + googleMapApiKey +
+                "&units=" + units +
+                "&avoid=" + avoids);
+            var request = new RestRequest(Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+
+            var json = response.Content;
+
+            try
+            {
+                var responseResult = JsonConvert.DeserializeObject<ResponseModel>(json);
+
+                lnOrigins.Text = responseResult.OriginAddresses[0];
+                lbDestination.Text = responseResult.DestinationAddresses[0];
+                lbDistance.Text = responseResult.Rows[0].Elements[0].Distance.Text;
+                lbTime.Text = responseResult.Rows[0].Elements[0].Duration.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("msg :: " + ex.InnerException, "Error");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrEmpty(textBox2.Text))
+            {
+                GetSingleDistanceFromGoogleMapApi(textBox1.Text, textBox2.Text);
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(textBox1.Text))
+                {
+                    textBox1.Focus();
+                }
+
+                if (string.IsNullOrEmpty(textBox2.Text))
+                {
+                    textBox2.Focus();
+                }
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            ReleaseCapture();
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class ResponseModel
+    {
+        public string Status { get; set; }
+
+        [JsonProperty(PropertyName = "destination_addresses")]
+        public string[] DestinationAddresses { get; set; }
+
+        [JsonProperty(PropertyName = "origin_addresses")]
+        public string[] OriginAddresses { get; set; }
+
+        public Row[] Rows { get; set; }
+
+        public class Data
+        {
+            public int Value { get; set; }
+            public string Text { get; set; }
+        }
+
+        public class Element
+        {
+            public string Status { get; set; }
+            public Data Duration { get; set; }
+            public Data Distance { get; set; }
+        }
+
+        public class Row
+        {
+            public Element[] Elements { get; set; }
+        }
+    }
+}
